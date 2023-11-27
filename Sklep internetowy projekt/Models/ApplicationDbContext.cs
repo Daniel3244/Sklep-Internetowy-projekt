2c482695dd7c9734198515853af182d05be7d224
Daniel <daniel.migas123@interia.pl>
2023-11-27 21:19:18 +0100

    Pierwszy commit

---
+++ Sklep internetowy projekt/Models/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sklep_internetowy_projekt.Models;
+using System.Collections.Generic;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+}
